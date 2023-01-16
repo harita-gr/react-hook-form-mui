73d0f8eba75838d19c485f81df722edf6ec7e560
Harita Ravindranath <haritagr19@gmail.com>
2023-01-16 08:20:52 +0530

    basic form

---
+++ src/components/BasicForm.tsx
@@ -0,0 +1,46 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IFormInputs {
+  email: string;
+  password: string;
+}
+
+const BasicForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IFormInputs>();
+
+  console.log("errors:", errors);
+  console.log("watch variable email:", watch("email"));
+
+  //TIP: You can access formState inside SubmitHandler. Pass "data" param.
+  const formSubmitHandler: SubmitHandler<IFormInputs> = (data: IFormInputs) => {
+    console.log("Form Data is:", data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(formSubmitHandler)}>
+      <input
+        type="email"
+        defaultValue="example@leo.test.com"
+        {...register("email")}
+      />
+      <br /> <br />
+      <input
+        type="password"
+        {...register("password", {
+          required: true,
+        })}
+      />
+      <br />
+      {errors.password && <span>This field is required</span>}
+      <br />
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default BasicForm;
